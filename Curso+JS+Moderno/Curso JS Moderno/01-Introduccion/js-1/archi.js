453c6c12080e11a85d4ab19f217b30db5b70dbcb
fedemv <fedemv93@gmail.com>
2023-07-28 14:04:46 -0300

    primer commit

---
+++ Curso+JS+Moderno/Curso JS Moderno/01-Introduccion/js-1/archi.js
@@ -0,0 +1,5 @@
+const nombre = prompt("Cual es tu nombre ?");
+
+
+//toma el nombre y lo muestra en pantalla 
+document.querySelector(".contenido").innerHTML = `${nombre} esta aprendiendo JS Moderno`;
---
+++ JavaScriptCode/variables/01-app.js
@@ -0,0 +1,29 @@
+//si un lenguaje tiene variables es un lenguaje si no NO 
+var producto = "monitor de 24 pulgadas"; //iniciar variable y asignarle valor
+producto = 'monitor de 19 pulgadas'; //reasignando el valor de la variable
+//JavaScript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 20; //reasignando el valor de la variable
+
+//se pueden inicializar sin valor y asignarlo despues
+var disponible;
+disponible = true;
+disponible = false;
+
+//inicializar multiples variables
+var categoria = 'computadoras',
+    marca = "marca famosa",
+    calificacion = 5;
+
+
+//las variables no pueden iniciar con numeros
+//var 99dias;
+var dias99;
+
+//var 01_;
+var _01;
+
+//estilos para las variables    
+var nombre_producto = 'Monitor HD'; //underscore
+var nombreProducto = 'Monitor HD'; //camelcase
+var NombreProducto = 'Monitor HD'; //pascal case //se usa para clases
+var nombreproducto = 'Monitor HD'; //lowercase //se usa para constantes
